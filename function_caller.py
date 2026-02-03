1f3298cd2d85abe21a3897b1cebf5af5bdc3209b
Nosheen Adil <nosheen.0398@gmail.com>
2026-02-03 08:56:28 -0800

    chore: remove unused files from v2 schema

--- function_caller.py
+++
@@ -1,6 +0,0 @@
-import sys
-
-from ideas.interface import run_tool
-
-if __name__ == "__main__":
-    run_tool(tool_command=sys.argv[1])
--- install_imported_code.sh
+++
@@ -1,3 +0,0 @@
-if [ -d /ideas/toolbox/tools/imported_code ]; then
-  pip install -e /ideas/toolbox/tools/imported_code
-fi
